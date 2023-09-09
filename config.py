60493a0aae93d8bba2620564e664a11a8a7440f7
Mauro M <hello@maurom.dev>
2023-09-09 13:19:42 -0500

    feat: fix Settings

--- config.py
+++ config.py
@@ -3,5 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     app_name: str = "LibreCharts API"
+    password: str
     update_token: str
+    sentry_uri: str
     model_config = SettingsConfigDict(env_file=".env")
